2a034ecc6e5d5c33a724f102fa0fa4a86888a625
financehznu <finance_hznu@163.com>
2019-09-18 14:56:47 +0800

    add page1 by zhuyiming

--- CreditSystem/urls.py
+++ CreditSystem/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from creditmanagement import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('page1/',views.page1)
 ]
